ae56670bc156e4e116c701af8dab1f1ee22b9bfc
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-04-05 20:52:22 -0700

    Se volvieron a poner los .equals a 2 clases que no lo tenian...

--- ObjetosNegocioBazar/src/objetosNegocio/Compra.java
+++ ObjetosNegocioBazar/src/objetosNegocio/Compra.java
@@ -101,6 +101,28 @@ public class Compra implements Serializable {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Compra))
+        {
+            return false;
+        }
+        Compra other = (Compra) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
     public String toString() {
         return "Compra{" + "id=" + id + ", cantidad=" + cantidad + ", precio=" + precioProducto + ", producto=" + producto + ", proveedor=" + proveedor + '}';
     }
--- ObjetosNegocioBazar/src/objetosNegocio/DetalleVenta.java
+++ ObjetosNegocioBazar/src/objetosNegocio/DetalleVenta.java
@@ -87,6 +87,28 @@ public class DetalleVenta implements Serializable {
     public float getPrecioProducto() {
         return this.precioProducto;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof DetalleVenta))
+        {
+            return false;
+        }
+        DetalleVenta other = (DetalleVenta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public String toString() {
