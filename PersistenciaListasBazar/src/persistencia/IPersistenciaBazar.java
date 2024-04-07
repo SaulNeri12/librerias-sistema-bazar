4d46226a7b0235ce140f6afcc567f73d1bec936b
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-04-06 23:10:41 -0700

    Se anadieron datos (productos) de prueba a la persistencia listas para ser utilizado en el mock...

---
+++ PersistenciaListasBazar/src/persistencia/IPersistenciaBazar.java
@@ -0,0 +1,23 @@
+
+package persistencia;
+
+import java.util.List;
+import objetosNegocio.Producto;
+import objetosNegocio.Usuario;
+import objetosNegocio.Venta;
+import persistencia.excepciones.PersistenciaBazarException;
+
+/**
+ * Contiene todas las operaciones necesarias para el sistema de ventas.
+ * @author saul
+ */
+public interface IPersistenciaBazar {
+    
+    public List<Producto> consultarProductosTodos() throws PersistenciaBazarException;
+    
+    public List<Producto> consultarProductosPorNombre(String nombreProducto) throws PersistenciaBazarException;
+    
+    public void registrarVenta(Venta venta) throws PersistenciaBazarException;
+    
+    public Usuario iniciarSesionUsuario(Usuario usuario) throws PersistenciaBazarException;
+}
